95ed9c648b33a7c6eb794ccb1ddddab6e498e795
Vilzz <160743963+Vilamuzz@users.noreply.github.com>
2026-01-30 16:25:05 +0700

    update .example.env

--- app/middleware/middleware.go
+++ app/middleware/middleware.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"io"
+	"os"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -52,7 +53,7 @@ func NewCORSMiddleware() *CORSMiddleware {
 
 func (m *CORSMiddleware) CORS() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
+		AllowOrigins:     []string{os.Getenv("CORS_ALLOW_ORIGIN")},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
