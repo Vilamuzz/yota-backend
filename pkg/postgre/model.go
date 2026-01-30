8890beb829dd158a3941305889dbf14ffb15abef
Vilzz <160743963+Vilamuzz@users.noreply.github.com>
2026-01-30 15:46:32 +0700

    fix: correct import statement for image package in model.go

--- pkg/postgre/model.go
+++ pkg/postgre/model.go
@@ -3,7 +3,7 @@ package postgre_pkg
 import (
 	"github.com/Vilamuzz/yota-backend/app/auth"
 	"github.com/Vilamuzz/yota-backend/app/donation"
-	image "github.com/Vilamuzz/yota-backend/app/image"
+	"github.com/Vilamuzz/yota-backend/app/image"
 	"github.com/Vilamuzz/yota-backend/app/news"
 	"github.com/Vilamuzz/yota-backend/app/social_program"
 	"github.com/Vilamuzz/yota-backend/app/user"
